1054b3474cbb001f94160337e1c3b2c4d7530a7e
vikassyadav <vikasyadav2702@gmail.com>
2024-11-25 00:24:01 +0530

    adde dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//    implementation ("com.budiyev.android:code-scanner:2.1.0")
+    implementation ("com.shreyaspatil:EasyUpiPayment:2.0")
 }
--- app/src/main/java/com/example/paytrack/MainActivity.java
+++ app/src/main/java/com/example/paytrack/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.paytrack;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         // Set Home as default fragment
         getSupportFragmentManager().beginTransaction()
